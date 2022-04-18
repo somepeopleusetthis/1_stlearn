40a5a481417e7d8b8f098548587ad175996f1d84
Natural <treg.vitalia@yandex.ru>
2022-04-18 15:54:39 +0300

    start

---
+++ cmake-build-debug/CMakeFiles/1_stlearn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1_stlearn.exe"
+  "1_stlearn.pdb"
+  "CMakeFiles/1_stlearn.dir/main.cpp.o"
+  "lib1_stlearn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_stlearn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<opencv2/core/cpre.hpp>
+
+#include <iostream>
+#include <conio.h>
+#include<math.h>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
